4809680ab89f5a88bcac14d8aed254516ea9e7cb
nirmala252m-afk <nirmala.252.m@gmail.com>
2025-11-24 22:13:44 +0530

    Add files via upload

---
+++ resume-builder/script.js
@@ -0,0 +1,6 @@
+// Example JS: Add your own interactivity here
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector('h1').onclick = function() {
+    alert("Welcome to My Website!");
+  };
+});
